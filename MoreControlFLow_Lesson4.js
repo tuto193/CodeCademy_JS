e0b29a77937b904050a172720085ec6b269f05f7
tuto193 <carlos.a.parra.f@gmail.com>
2016-06-05 17:17:55 +0200

    Here we carry on just now

---
+++ MoreControlFLow_Lesson4.js
@@ -0,0 +1,18 @@
+var lunch = prompt("What do you want for lunch?","Type your lunch choice here");
+
+switch(lunch){
+  case 'sandwich':
+    console.log("Sure thing! One sandwich, coming up.");
+    break;
+  case 'soup':
+    console.log("Got it! Tomato's my favorite.");
+    break;
+  case 'salad':
+    console.log("Sounds good! How about a caesar salad?");
+    break;
+  case 'pie':
+    console.log("Pie's not a meal!");
+    break;
+  default:
+    console.log("Huh! I'm not sure what " + lunch + " is. How does a sandwich sound?");
+}
