a6d1b2ce95131a7d3bc500a12ae15dad7b896899
tuto193 <carlos.a.parra.f@gmail.com>
2016-06-24 22:57:54 +0200

    Heterogeneous arrays starting

---
+++ DoneOnes/ArraysAndObjectsInJS_L11.js
@@ -0,0 +1,6 @@
+/* And here we start with the lesson 11 */
+
+var me = new Object();
+
+me.name = "Carlos";
+me.age = 22;
---
+++ DoneOnes/ArraysAndObjectsInJS_L12.js
@@ -0,0 +1,9 @@
+/* Lesson 12 hereeeee... bleeee */
+
+var object1 = new Object();
+var object2 = new Object();
+var object3 = new Object();
+
+object1.name = "Poopy";
+object2.name = "Doopy";
+object3.name = "Doo";
